820c55b63d7c31bd816da795f48a982623a15bd3
Jaime Ramos <jaime.ramos1@alu.uclm.es>
2018-10-15 11:57:22 +0200

    Updated 1510

--- cliente/src/modelo.js
+++ cliente/src/modelo.js
@@ -6,23 +6,25 @@ function Juego(){
 		this.cartas.push(carta);
 	}
 	this.agregarUsuario=function(usuario){
-		usuario.mazo=this.cartas;
+		usuario.mazo=_.shuffle(this.crearColeccion());
 		usuario.juego=this;
 		this.usuarios.push(usuario);
 	}
 	this.crearColeccion=function(){
+		var mazo=[];
 		//10 ataque 5 coste 3 vida 5
 		for (var i=0;i<10;i++){
-			this.cartas.push(new Carta("Dragon"+i, 5, 5,3));
+			mazo.push(new Carta("Dragon"+i, 5, 5,3));
 		}
 		//10 ataque 3 coste 2 vida 3
 		for (var i=0;i<10;i++){
-			this.cartas.push(new Carta("Guerrero"+i, 3, 3,2));
+			mazo.push(new Carta("Guerrero"+i, 3, 3,2));
 		}
 		//10 ataque 2 coste 1 vida 2
 		for (var i=0;i<10;i++){
-			this.cartas.push(new Carta("Esbirro"+i, 2, 2,1));
+			mazo.push(new Carta("Esbirro"+i, 2, 2,1));
 		}
+		return mazo;
 	}
 	this.agregarPartida=function(partida){
 		this.partidas.push(partida);
@@ -40,18 +42,49 @@ function Juego(){
 		}
 	}
 	//aquí se construye el Juego
-	this.crearColeccion();
+	//this.crearColeccion();
 }
 
 function Partida(nombre){
 	this.nombre=nombre;
+	this.usuariosPartida=[];
 	this.tablero=undefined;
 	this.crearTablero=function(){
 		this.tablero=new Tablero();
 	}
 	this.asignarUsuario=function(usuario){
 		usuario.asignarPartida(this);
+		this.usuariosPartida.push(usuario);
 		this.tablero.asignarUsuario(usuario);
+		this.comprobarInicio();
+	}
+	this.comprobarInicio=function(){
+		if (this.usuariosPartida.length==2){
+			this.turnoInicial();
+			this.asignarManoInicial();
+		}
+	}
+	this.asignarManoInicial=function(){
+		for(var i=0;i<this.usuariosPartida.length;i++){
+			this.usuariosPartida[i].manoInicial();
+		}
+	}
+	this.turnoInicial=function(){
+		var num=Math.round(Math.random());
+		this.usuariosPartida[num].esMiTurno();
+
+	}
+	this.cambiarTurno=function(){
+		for(var i=0;i<this.usuariosPartida.length;i++){
+			this.usuariosPartida[i].cambiarTurno();
+		}
+		// if (this.usuariosPartida[0].turno){
+		// 	this.usuariosPartida[0].turno=false;
+		// 	this.usuariosPartida[1].esMiTurno();
+		// }else{
+		// 	this.usuariosPartida[1].turno=false;
+		// 	this.usuariosPartida[0].esMiTurno();
+		// }
 	}
 	this.crearTablero();
 }
@@ -94,13 +127,55 @@ function Zona(nombre){
 	}
 }
 
+function MiTurno(){
+	this.pasarTurno=function(usr){
+		usr.partida.cambiarTurno();
+	}
+	this.jugarCarta=function(usr,carta){
+		usr.puedeJugarCarta(carta);
+	}
+	this.cambiarTurno=function(usr){
+		usr.turno=new NoMiTurno();
+	}
+	this.meToca=function(){
+		return true;
+	}
+}
+
+function NoMiTurno(){
+	this.esMiTurno=function(usr){
+		usr.turno=new MiTurno();
+		usr.cogerCarta();
+		usr.elixir=usr.consumido+1;
+		usr.consumido=0;
+	}
+	this.pasarTurno=function(usr){
+		console.log("No se puede pasar el turno si no se tiene");
+	}
+	this.jugarCarta=function(carta,usr){
+		console.log("No es tu turno");
+	}
+	this.cambiarTurno=function(usr){
+		usr.turno=new MiTurno();
+		this.esMiTurno(usr);
+	}
+	this.meToca=function(){
+		return false;
+	}
+}
+
 function Usuario(nombre){
 	this.nombre=nombre;
 	this.juego=undefined;
+	this.vidas=20;
 	this.mazo=[];
-	this.mano=[];
+	//this.mano=[];
+	//this.ataque=[];
+	this.elixir=1;
+	this.turno=new NoMiTurno();
 	this.zona=undefined;
 	this.partida=undefined;
+	this.consumido=0;
 	this.asignarPartida=function(partida){
 		this.partida=partida;
 	}
@@ -113,6 +188,61 @@ function Usuario(nombre){
 	this.eligePartida=function(nombre){
 		this.juego.asignarPartida(nombre,this);
 	}
+	this.cambiarTurno=function(){
+		this.turno.cambiarTurno(this);
+	}
+	this.pasarTurno=function(){
+		//this.partida.cambiarTurno();
+		this.turno.pasarTurno(this);
+	}
+	this.esMiTurno=function(){
+		this.turno.esMiTurno(this);
+		// this.turno=true;
+		// this.cogerCarta();
+		// this.elixir=this.consumido+1;
+		// this.consumido=0;
+	}
+	this.cogerCarta=function(){
+		var carta;
+		carta= this.mazo.find(function(each){
+			return each.posicion=="mazo";
+		});
+		carta.posicion="mano";
+	}
+	this.jugarCarta=function(carta){
+		this.turno.jugarCarta(this,carta);
+	}
+	this.puedeJugarCarta=function(carta){
+		if (this.elixir>=carta.coste){
+			carta.posicion="ataque";
+			this.elixir=this.elixir-carta.coste;
+			this.consumido=this.consumido+carta.coste;
+		}
+		else
+			console.log("No tienes suficiente elixir");
+	}
+	this.ataque=function(carta,objetivo){
+		objetivo.esAtacado(carta);
+	}
+	this.esAtacado=function(carta){
+		this.vidas=this.vidas-carta.ataque;
+		this.comprobarVidas();
+	}
+	this.comprobarVidas=function(){
+		if (this.vidas<=0){
+			this.juego.fin();
+		}
+	}
+	this.manoInicial=function(){
+		for(var i=0;i<5;i++){
+			this.cogerCarta();
+		}
+	}
+	this.localizarCarta=function(coste){
+		return this.mazo.find(function(each){
+			return each.posicion=="mano" && each.coste==coste;
+		});
+	}
 }
 
 function Carta(vidas,ataque,nombre,coste){
@@ -120,4 +250,15 @@ function Carta(vidas,ataque,nombre,coste){
 	this.ataque=ataque;
 	this.nombre=nombre;
 	this.coste=coste;
+	this.posicion="mazo";
+	this.esAtacado=function(carta){
+		this.vidas=this.vidas-carta.ataque;
+		this.comprobarVidas();
+		carta.comprobarVidas();
+	}
+	this.comprobarVidas=function(){
+		if (this.vidas<=0){
+			this.posicion="cementerio";
+		}
+	}
 }
--- servidor/modeloSpec.js
+++ servidor/modeloSpec.js
@@ -1,42 +1,90 @@
 var modelo = require('./modelo.js');
 describe("El juego de las cartas...", function() {
   var juego;
-  var usr;
-  
+  var usr1,usr2;
+
   beforeEach(function() {
-    juego = new modelo.Juego();
-    usr=new modelo.Usuario("Sandra");
-    usr2 = new modelo.Usuario("Juan");
-  });
-
-   it("compruebo condiciones iniciales (cartas, usuarios)", function() {
-    expect(juego.cartas).toBeDefined();
-    expect(juego.cartas.length).toEqual(30);
-    expect(juego.usuarios).toBeDefined();
-    expect(juego.usuarios.length).toEqual(0);
-  });
-
-  it("el usuario tiene un mazo",function(){
-    expect(usr.mazo).toBeDefined();
-    expect(usr.mazo.length).toEqual(0);
-    expect(usr2.mazo).toBeDefined();
-    expect(usr2.mazo.length).toEqual(0);
-  });
-
-  it("el usuario tiene mano",function(){
-    expect(usr.mano).toBeDefined();
-    expect(usr.mano.length).toEqual(0);
-    expect(usr2.mano).toBeDefined();
-    expect(usr2.mano.length).toEqual(0);
-  });
-
-  it("agrego varios usuarios al juego",function(){
-    juego.agregarUsuario(usr);
+    juego=new modelo.Juego();
+    usr1=new modelo.Usuario("pepe");
+    usr2=new modelo.Usuario("juan");
+    miturno=new modelo.MiTurno();
+    nomiturno=new modelo.NoMiTurno();
+    juego.agregarUsuario(usr1);
     juego.agregarUsuario(usr2);
-    expect(juego.usuarios.length).toEqual(2);
-    expect(juego.usuarios[0].nombre).toEqual("Sandra");
-    expect(juego.usuarios[1].nombre).toEqual("Juan");
-    expect(usr.mazo.length).toEqual(30);
-    expect(usr2.mazo.length).toEqual(30);
+    usr1.crearPartida("prueba");
+    usr2.eligePartida("prueba");
   });
+
+   it("Compruebo condiciones iniciales (cartas, partidas, usuario)", function() {
+    expect(juego.usuarios).toBeDefined();
+    expect(juego.usuarios.length).toEqual(2);
+    expect(juego.partidas).toBeDefined();
+    expect(juego.partidas.length).toEqual(1);
+  });
+
+  it("Los usuarios tienen un mazo", function(){
+    expect(usr1.mazo).toBeDefined();
+    expect(usr1.mazo.length).toEqual(30);
+    expect(usr2.mazo).toBeDefined();
+    expect(usr2.mazo.length).toEqual(30);
+    });
+
+    it("Los usuarios tiene mano (inicialmente sin cartas)", function(){
+      var cont=0;
+      for(var i=0;i<usr1.mazo.length;i++){
+        if (usr1.mazo[i].posicion=="mano"){
+          cont++
+        }
+      }
+      expect(cont).toBeGreaterThan(4);
+      expect(cont).toBeLessThan(7);
+    });
+
+   it("agregar pepe y juan el usuario al juego", function(){
+    //juego.agregarUsuario(usr1);
+    //juego.agregarUsuario(usr2);
+    expect(juego.usuarios.length).toEqual(2);
+    expect(juego.usuarios[0].nombre).toEqual("pepe");
+    expect(usr1.mazo.length).toEqual(30);
+    expect(juego.usuarios[1].nombre).toEqual("juan");
+    expect(usr2.mazo.length).toEqual(30);
+    });
+
+   it("Pepe crea una partida, juan la elige y se les asigna las zonas correspondientes", function(){
+      expect(juego.usuarios[0].partida.nombre).toEqual("prueba");
+      expect(usr1.partida.nombre).toEqual("prueba");
+      expect(juego.usuarios[1].partida.nombre).toEqual("prueba");
+      expect(juego.usuarios[0].zona.nombre).toEqual("arriba");
+      expect(juego.usuarios[1].zona.nombre).toEqual("abajo");
+      expect(usr1.partida.usuariosPartida.length).toEqual(2);
+      if (usr1.turno.meToca()){
+        expect(usr2.turno.meToca()).toBe(false);
+      }
+      else{
+        expect(usr2.turno.meToca()).toBe(true);
+      }
+    });
+
+   it("Comprobar que funciona pasar turno",function(){
+      usr1.turno=miturno;
+      usr2.turno=nomiturno;
+      usr1.pasarTurno();
+      expect(usr1.turno.meToca()).toEqual(false);
+      expect(usr2.turno.meToca()).toEqual(true);
+   });
+
+   it("Al jugar una carta, la carta pasa a la zona de ataque y se decrementa el elixir en 1",function(){
+      //Forzamos el turno para el usr1
+      usr1.turno=miturno;
+      usr2.turno=nomiturno;
+      //Localizamos una carta de coste 1
+      var carta=usr1.localizarCarta(1);
+      if (carta!=undefined){
+        usr1.jugarCarta(carta);
+        expect(usr1.elixir).toEqual(0);
+        expect(usr1.consumido).toEqual(1);
+        expect(carta.posicion).toEqual("ataque");
+      }
+   });
+
 });
